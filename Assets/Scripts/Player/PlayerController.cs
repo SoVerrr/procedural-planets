dfd75e830c57965758c9bf0acb869968b2740fe3
SoVerrr <82769474+SoVerrr@users.noreply.github.com>
2023-11-14 12:46:35 +0100

    Basic gravity, player object
    Added player object, rotating it according to players center and basic gravity.
    Need to fix sliding on planet surface

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float speed;
+    [SerializeField] private float gravityForce;
+    [SerializeField] private ForceMode forceMode;
+
+    private Rigidbody rigidbody;
+
+    private void Awake()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Gravity()
     {
-        
+        Vector3 centrePoint = new Vector3(Values.Instance.PlanetSize.x / 2, Values.Instance.PlanetSize.y / 2, Values.Instance.PlanetSize.z / 2);
+        Vector3 g = (centrePoint - transform.position) * gravityForce;
+        rigidbody.velocity = g * Time.deltaTime;
+        Vector3 localUp = rigidbody.rotation * Vector3.up;
+        Vector3 gravityUp = (transform.position - centrePoint).normalized;
+        transform.rotation = Quaternion.FromToRotation(localUp, gravityUp) * rigidbody.rotation;
     }
+
+    private void Update()
+    {
+        Gravity();
+    }
+
 }
